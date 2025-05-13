817efe4a11179b9d814c91700b99c2280ea539f8
vinimigd <165157937+vinimigd@users.noreply.github.com>
2025-05-13 10:18:20 -0300

    as

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
@@ -61,20 +62,21 @@ function geraSenha() {
 
 }
 
-function classificaSenha(tamanhoAlfabeto){
+function classificaSenha(tamanhoAlfabeto) {
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     console.log(entropia);
-    forcaSenha.classList.remove('fraca','media','forte');
-    if (entropia > 57){
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (entropia > 57) {
         forcaSenha.classList.add('forte');
-    } else if (entropia > 35 && entropia < 57 ) {
+    } else if (entropia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
-    } else if (entropia <= 35){
+    } else if (entropia <= 35) {
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha.";
 }
+
 
 
 
